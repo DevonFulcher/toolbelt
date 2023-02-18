d7fb90e88c248a5ba94ad322d7bd579e38e0882b
Devon Fulcher <devonfulcher3@gmail.com>
2023-02-18 17:21:38 -0600

    added git save

--- main.go
+++ main.go
@@ -3,8 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
+func Equal[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	fmt.Println(os.Args[1:])
+	og := os.Args[1:]
+	if Equal(og[:2], []string{"git", "save"}) {
+		cmds := [][]string{
+			{"git", "add", "-A"},
+			{"git", "commit", "-m", og[2]},
+			{"git", "push"},
+		}
+		for _, cmd := range cmds {
+			fmt.Println(cmd)
+			toRun := exec.Command(cmd[0], cmd[1:]...)
+			toRun.Stdout = os.Stdout
+			if err := toRun.Run(); err != nil {
+				fmt.Println("could not run command: ", err)
+			}
+		}
+	}
 }
