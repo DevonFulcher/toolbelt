7ba409615cd5978b7809a35b9b0c13f30de53510
Devon Fulcher <devonfulcher3@gmail.com>
2023-06-23 09:52:39 -0500

    updated path to vscode dotfile

--- config/config.go
+++ config/config.go
@@ -15,7 +15,8 @@ var REPOS_PATH = path.Join(HOME, "git")
 var CLI_PATH = path.Join(HOME, "cli")
 var SLG_PATH = path.Join(REPOS_PATH, SLG_REPO)
 var RUNTIME_GATEWAY_PATH = path.Join(REPOS_PATH, RUNTIME_GATEWAY_REPO)
+var DOTFILES_PATH = path.Join(REPOS_PATH, DOTFILES_REPO)
 
 // https://code.visualstudio.com/docs/getstarted/settings#_settings-file-locations
-var VSCODE_DOTFILES_SETTINGS = path.Join(REPOS_PATH, "shared/vscode/settings.json")
+var VSCODE_DOTFILES_SETTINGS = path.Join(DOTFILES_PATH, "shared/vscode/settings.json")
 var VSCODE_USER_SETTINGS = path.Join(HOME, "Library/Application Support/Code/User/settings.json")
