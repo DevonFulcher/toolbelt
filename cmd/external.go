6276504b735dc94d61a62482dabe72c7146f79b6
Devon Fulcher <devonfulcher3@gmail.com>
2023-10-30 22:47:48 -0500

    update login

--- cmd/external.go
+++ cmd/external.go
@@ -92,7 +92,7 @@ var CmdTree = []External{
 		name:        "morning",
 		description: "morning script",
 		run: func(params []string) error {
-			c := New("fsh login")
+			c := New("aws sso login")
 			_, err := c.RunCmd()
 			if err != nil {
 				return err
@@ -137,11 +137,15 @@ var CmdTree = []External{
 				name:        "reset",
 				description: "reset devspace",
 				run: func(params []string) error {
+					err := os.Remove("~/.devspace")
+					if err != nil {
+						return err
+					}
 					cmds := []Internal{
 						New("fsh dev destroy %v", config.DEVSPACE_NAMESPACE),
 						New("devspace use namespace %v", config.DEVSPACE_NAMESPACE),
 					}
-					_, err := RunCmds(cmds)
+					_, err = RunCmds(cmds)
 					if err != nil {
 						return err
 					}
